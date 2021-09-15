3701656f98e55381460b60078b0bee3ef3a33094
Monke001 <rharanru@gmail.com>
2021-09-15 15:51:01 -0300

    default for heroku

--- config/settings.py
+++ config/settings.py
@@ -91,15 +91,6 @@ CHANNEL_LAYERS = {
     },
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get('REDIS_URL'),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 

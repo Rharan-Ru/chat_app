4aef80a76c1499efbe834d9607e2ae4c6234ace5
Monke001 <rharanru@gmail.com>
2021-09-15 15:09:42 -0300

    default for heroku

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis', 6379)],
+            "hosts": [(os.environ.get('REDIS_URL'), 6379)],
         },
     },
 }
